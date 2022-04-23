f44ae25da4e09fe53a26456cf4de14b6d4886d74
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-23 06:49:05 -0600

    Added pixabay image as background

--- src/VendingMachine/VendingMachine.jsx
+++ src/VendingMachine/VendingMachine.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './VendingMachine.css';
+
 const VendingMachine = () => {
     return ( 
-    <>
+    <div className="Vending">
     I am a vending machine
-    </>
+    </div>
      );
 }
  
