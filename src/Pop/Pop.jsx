1ede443403f82fb9c4d88766688afceac7e05dad
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-23 07:07:39 -0600

    Added pop background

--- src/Pop/Pop.jsx
+++ src/Pop/Pop.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Pop.css';
+
 const Pop = () => {
-    return ( 
-        <>
-        Future Pop
-        </>
-     );
-}
- 
+  return (
+    <div className="Pop">
+      Future Pop
+      <button>
+        <Link to="/">Gimme more</Link>
+      </button>
+    </div>
+  );
+};
+
 export default Pop;
