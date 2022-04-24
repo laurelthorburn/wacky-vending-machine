57f0f74cb40f4fc76bb05e917a5b2070b5182b2b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-24 07:32:11 -0600

    Added pop rotate animation

--- src/Pop/Pop.jsx
+++ src/Pop/Pop.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Pop.css';
+import Cola from '../media/cola.png';
+import Lime from '../media/lime.png';
+import Orange from '../media/orange.png';
 
 const Pop = () => {
   return (
     <div className="Pop">
-      Future Pop
+        <div className="Pop-imgs">
+        <img src={Cola} alt="cola pop" className="Pop-img" />
+        <img src={Lime} alt="lime pop" className="Pop-img" />
+        <img src={Orange} alt="orange pop" className="Pop-img" />
+        </div>
       <button>
         <Link to="/">Gimme more</Link>
       </button>
