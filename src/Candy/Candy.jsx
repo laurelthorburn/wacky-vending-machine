eaf24909ceecbeb05ee84bce73673b33f18e0e1d
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-25 06:33:10 -0600

    Creating cough pg

--- src/Candy/Candy.jsx
+++ src/Candy/Candy.jsx
@@ -1,26 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Candy.css";
 
 const Candy = () => {
+  const [candy, setCandy] = useState(0);
 
-    const [candy, setCandy] = useState(0);
+  const handleIncrement = () => {
+    let candyTotal = candy;
+    setCandy(candyTotal + 1);
+  };
 
-    const handleIncrement = () => {
-        let candyTotal = candy;
-        setCandy(candyTotal + 1);
-    }
-
-    return ( 
-        <div className="Candy">
-        total consumed Candy: {candy}
-        <button className="Candy-more" onClick={handleIncrement}>
-            I WANT MORE CANDYYYYY
-        </button>
-        <button>
+  return (
+    <div className="Candy">
+      total consumed candy: {candy}
+      <button className="Candy-more" onClick={handleIncrement}>
+        I WANT MORE CANDYYYYY
+      </button>
+      <button>
         <Link to="/">Gimme more</Link>
       </button>
-        </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default Candy;
--- src/CoughSyrup/CoughSyrup.jsx
+++ src/CoughSyrup/CoughSyrup.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Nyquil from '../media/nyquil.png';
+import './CoughSyrup.css'
 
 const CoughSyrup = () => {
     return ( 
-        <>
-        Future CoughSyrup
+        <div className="CoughSyrup">
+        I got the shakes, 
+        Nyquil Shakes
+        <img src={Nyquil} alt="Nyquil Cough Syrup" className="CoughSyrup-shake" />
         <button>
         <Link to="/">Gimme more</Link>
       </button>
-        </>
+        </div>
      );
 }
  
